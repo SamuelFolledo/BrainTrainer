ca8f5ae9186556e616ff5b450e124ea9ad610674
Samuel Folledo <samuelfolledo@gmail.com>
2019-11-20 12:42:15 -0800

    [add] setup initial environment

---
+++ BrainTrainer/Controller/MainVC.swift
@@ -0,0 +1,30 @@
+//
+//  MainVC.swift
+//  BrainTrainer
+//
+//  Created by Macbook Pro 15 on 11/20/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BrainTrainer/Model/Question.swift
@@ -0,0 +1,9 @@
+//
+//  Question.swift
+//  BrainTrainer
+//
+//  Created by Macbook Pro 15 on 11/20/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import Foundation
