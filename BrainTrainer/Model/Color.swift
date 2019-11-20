7bfbf7dbe663f859b10240ed4179a036eb505987
Samuel Folledo <samuelfolledo@gmail.com>
2019-11-20 14:25:53 -0800

    [add] a way to update color by adopting and conforming to case iterable protocol which allowed us use allCases

--- BrainTrainer/Model/Color.swift
+++ BrainTrainer/Model/Color.swift
@@ -6,8 +6,31 @@
 //  Copyright © 2019 SamuelFolledo. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 enum Color {
     case red, orange, yellow, green, blue, purple
+    
+    var currentColor: UIColor {
+        switch self {
+        case .red:
+            return .red
+        case .orange:
+            return .orange
+        case .yellow:
+            return .yellow
+        case .green:
+            return .green
+        case .blue:
+            return .blue
+        case .purple:
+            return .purple
+        }
+    }
+}
+
+extension Color: CaseIterable {
+    mutating func updateAsRandomColor() { //updates color to a random Color from all Color cases //CaseIterable allows me to use the allCases typeProperty
+        self = Color.allCases[Int(arc4random_uniform(UInt32(Color.allCases.count)))]
+    }
 }
