f39df92acbfe10c5420b80eafc4bea2d5afcbf8a
Samuel Folledo <samuelfolledo@gmail.com>
2019-11-20 14:33:28 -0800

    [add] Equatable protocol which checks if two color instances's text are the same

--- BrainTrainer/Model/Color.swift
+++ BrainTrainer/Model/Color.swift
@@ -11,7 +11,7 @@ import UIKit
 enum Color {
     case red, orange, yellow, green, blue, purple
     
-    var currentColor: UIColor {
+    var textColor: UIColor {
         switch self {
         case .red:
             return .red
@@ -27,10 +27,33 @@ enum Color {
             return .purple
         }
     }
+    
+    var text: String {
+        switch self {
+        case .red:
+            return "red"
+        case .orange:
+            return "orange"
+        case .yellow:
+            return "yellow"
+        case .green:
+            return "green"
+        case .blue:
+            return "blue"
+        case .purple:
+            return "purple"
+        }
+    }
 }
 
 extension Color: CaseIterable {
     mutating func updateAsRandomColor() { //updates color to a random Color from all Color cases //CaseIterable allows me to use the allCases typeProperty
         self = Color.allCases[Int(arc4random_uniform(UInt32(Color.allCases.count)))]
+    }
+}
+
+extension Color: Equatable {
+    static func ==(lhs: Color, rhs: Color) -> Bool { //checks if Color's text are the same
+        return lhs.text == rhs.text //&& lhs.age == rhs.age
     }
 }
