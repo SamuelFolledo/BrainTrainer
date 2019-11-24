7eff4f3d63e2c6999a1cd68414ad1340f37c768a
Samuel Folledo <samuelfolledo@gmail.com>
2019-11-24 00:00:43 -0800

    [add] fixed fadeIn and fadeOut tabbing

--- BrainTrainer/Helper/extensions/UIView+extensions.swift
+++ BrainTrainer/Helper/extensions/UIView+extensions.swift
@@ -24,12 +24,13 @@ extension UIView {
     func fadeIn(duration: TimeInterval = 1.0) {
         UIView.animate(withDuration: duration, animations: {
           self.alpha = 1.0
-      })
+        })
     }
+    
     func fadeOut(duration: TimeInterval = 1.0) {
         UIView.animate(withDuration: duration, animations: {
           self.alpha = 0.0
-      })
+        })
     }
     
     func applyShadow() {
