9a3abe9d6efe9b4e7c3524fb0f1d5a8e4b99a69b
Samuel Folledo <samuelfolledo@gmail.com>
2019-11-28 12:32:36 -0800

    [add] UIView swift file for Tutorial View

---
+++ BrainTrainer/View/TutorialView/TutorialView.swift
@@ -0,0 +1,77 @@
+//
+//  TutorialView.swift
+//  BrainTrainer
+//
+//  Created by Macbook Pro 15 on 11/28/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class TutorialView: UIView {
+    var color: Color {
+        didSet {
+            colorLabel.textColor = color.textColor
+        }
+    }
+    var text: Color {
+        didSet {
+            colorLabel.text = text.text
+        }
+    }
+    var cardColor: CardColor {
+        didSet {
+            backgroundView.backgroundColor = cardColor.color
+            switch cardColor {
+            case .white, .green, .red:
+                while colorLabel.textColor == cardColor.color { //Error check: we dont want the colorLabel's textColor to be the same as the background, so we will update color to a new Color which will update the colorLabel's textColor
+                    color = Color()
+                }
+            case .black:
+                if colorLabel.textColor == cardColor.color { //we need this check because we do not want to change bottom card's textColor to be white
+                    colorLabel.textColor = .white
+                }
+            }
+        }
+    }
+//MARK: IBOutlets
+    @IBOutlet weak var videoView: UIView!
+    @IBOutlet weak var instructionLabel: UILabel!
+    
+    override init(frame: CGRect) { //for programmatically
+        cardColor = CardColor.white
+        color = Color()
+        text = Color()
+        super.init(frame: frame)
+        initializeXibFile()
+        setupView() //idk why it is required
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        cardColor = CardColor.white
+        color = Color()
+        text = Color()
+        super.init(coder: aDecoder)
+        initializeXibFile()
+        setupView() //required or it won't update colorLabel
+    }
+    
+    func setupView() {
+        videoView.backgroundColor = cardColor.color
+        instructionLabel.text = text.text
+        instructionLabel.textColor = color.textColor
+        self.isOpaque = false
+        self.applyShadow()
+    }
+
+    func initializeXibFile() {
+        let bundle = Bundle.init(for: CardView.self)
+        if let viewsToAdd = bundle.loadNibNamed("CardView", owner: self, options: nil), let contentView = viewsToAdd.first as? UIView {
+            addSubview(contentView)
+            contentView.frame = self.bounds
+            contentView.autoresizingMask = [.flexibleHeight,
+                                            .flexibleWidth]
+        }
+    }
+}
+
